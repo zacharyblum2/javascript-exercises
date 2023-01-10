d69f07a6b8c6df953dd5c2aca9b7f988c096a10c
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-10 14:39:37 -0500

    Created solution for tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // Round to one decimal place. 
+  let result = (temp - 32) * (5/9); 
+
+  return Math.round(result * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * (9/5) + 32);
+
+  return Math.round(result * 10)/10;
 };
 
 // Do not edit below this line
