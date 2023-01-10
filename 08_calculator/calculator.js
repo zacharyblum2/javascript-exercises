f3461aeb16a8130cd3fa1c4ff04e2d9eb98f0e8c
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-10 14:43:59 -0500

    Complete 5/6 basic calculator functions of calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,42 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0; 
+
+  for (let i = 0; i < array.length; i++) {
+    sum+= array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let sum = 0; 
 
+  for (let i = 0; i < array.length; i++) {
+    sum *= array[i];
+  }
+
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  if (power == 0)
+    return 1; 
+
+	let retval = base; 
+
+  for (let i = 0; i < power - 1; i++) {
+    retval *= base; 
+  }
+
+  return retval;
 };
 
 const factorial = function() {
