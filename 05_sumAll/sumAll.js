a75dbccd12821fe46a06842d3372a24a401b5269
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-10 14:24:32 -0500

    Create solution for sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
 
+    // If we are not adding numbers return an error. 
+    if (typeof(n1) != "number" || typeof(n2) != "number")
+        return 'ERROR';
+
+    let sum = 0, start = 0, end = 0;
+
+    if (n1 < n2) {
+        start = n1; 
+        end = n2; 
+    }
+    else {
+        start = n2; 
+        end = n1;
+    }
+    
+    for (let i = start; i <= end; i++) {
+        sum += i; 
+    }
+
+    if (sum < 0)
+        return 'ERROR';
+    else
+        return sum;
 };
 
 // Do not edit below this line
