8ba0c3c192c7d4e2cdc3b604d13f5b2fbedc543d
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-10 14:26:41 -0500

    Create solution for leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Leap Year: Divisible by four and not divisible by 100 unless divisible by 400
 
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            
+            return false;
+        }
+
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
