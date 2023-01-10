0f68c2d9bcc65b719495174dececb553b80f0962
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-09 19:14:11 -0500

    Solve 03_reverseString with two methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Reverse a string.
 
+    // 1. Using functions
+    // Split the string to return an array. 
+    let split = string.split("");
+
+    // Use reverse() to reverse the array. 
+    let reverse = split.reverse();
+
+    // Use Join to join the array as a string.
+    let retval = reverse.join("");
+
+    return retval;
+
+    // 2. Using for loop (first idea)
+    // let newString = "";
+    // for (let i = string.length - 1; i >= 0; i--)
+    //     newString += string[i];
+
+    // return newString;
 };
 
 // Do not edit below this line
