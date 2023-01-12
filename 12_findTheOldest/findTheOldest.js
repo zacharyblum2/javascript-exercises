d5d5b5e4c1b5601c56defe739f7c96fd8e4cddd4
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-11 21:05:02 -0500

    Create findTheOldest.js solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Given array of people with name, birth year, death year, find the oldest person. 
+    let max = 0, index = 0, age = 0;
+    let currYear = new Date().getFullYear();
 
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath == undefined)
+            age = currYear - people[i].yearOfBirth;
+        else 
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+
+        if (max < age) {
+            max = age;
+            index = i;
+        }        
+    }
+
+    return people[index];
 };
 
 // Do not edit below this line
