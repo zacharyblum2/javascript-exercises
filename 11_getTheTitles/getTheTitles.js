a0a0c91f54bc94fec3c995f5ebd1c0f09a16da26
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-11 20:53:57 -0500

    Create solution for getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // Given arry that stores two item arrays (book title and author), return array of titles.
+    const titles = []
 
+    for (let i = 0; i < books.length; i++) {
+        titles[i] = books[i].title;
+    }
+
+    return titles;
 };
 
 // Do not edit below this line
