463457d020aa505398004d6dc6eabad5a1a06c6d
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-11 20:41:31 -0500

    Solve palindromes.js using regex and built in functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Idea: Create a copy of the string but reverse it. Then compare directly to original. 
 
+    // Convert string to all lowercase and replace all non alphabetic characters with ""
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (processedString.split("").reverse().join("") == processedString);
 };
 
 // Do not edit below this line
