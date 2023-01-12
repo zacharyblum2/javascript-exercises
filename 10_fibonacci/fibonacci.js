c936afe8913648dd8f1f612bccdd187c53db2d6a
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-11 20:46:52 -0500

    Create array solution to fibbonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0 )
+        return "OOPS";
+    // Unsure how to create mutable array. Create one of size n. 
+    const fib = [];
+    fib[0] = 0; 
+    fib[1] = 1; 
+
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[n];
 };
 
 // Do not edit below this line
